fbe03900d0aca80f559fb306672ca1fe369f8cd4
DIKY ADISAPUTRA <68739579+dikyadisaputra@users.noreply.github.com>
2026-02-26 11:57:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿const links = document.querySelectorAll('.link-item');
+
+links.forEach((link, index) => {
+  link.style.transitionDelay = `${80 + index * 60}ms`;
+  requestAnimationFrame(() => {
+    link.classList.add('visible');
+  });
+});
